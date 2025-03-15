d550ece296f992e0315087720742929c0f7d46ee
Geoff <geoffduk@gmail.com>
2025-03-16 10:31:39 +1300

    cooked

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -2,6 +2,9 @@ extends Area3D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
+func _ready() -> void:
+	rotate_y(randi_range(0,360))
+
 func _physics_process(delta: float) -> void:
 	if (position.distance_to(player.position) > 100):
 		queue_free()
