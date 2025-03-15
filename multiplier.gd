8973a01614f24654c65d51c8c2b447586c89d671
Cameron <cben560@aucklanduni.ac.nz>
2025-03-15 20:16:00 +1300

    Multiplier rounding

--- multiplier.gd
+++ multiplier.gd
@@ -3,4 +3,4 @@ extends Label
 @onready var multiplier: Label = $"."
 
 func _process(delta):
-	multiplier.text = " Multiplier: %s " % Global.speed_score_multiplier
+	multiplier.text = " Multiplier: %s " % snapped(Global.speed_score_multiplier, 0.01)
