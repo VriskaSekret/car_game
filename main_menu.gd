62eb3472284af9ceac4fa52c97f0d776220bef5c
tabyrocket <abhijotmalhi@gmail.com>
2025-03-16 11:26:31 +1300

    Added sfx, UI controller support, and BGM

--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,32 @@
 extends Control
 
+@onready var start_button: Button = $VBoxContainer/startButton
+@onready var exit_button: Button = $VBoxContainer/exitButton
+
+func _ready() -> void:
+	BGM.play()
+	start_button.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/carSelect.tscn")
+	ButtonSFX.play()
+
+
+func _on_start_button_mouse_entered() -> void:
+	start_button.grab_focus()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_start_button_focus_entered() -> void:
+	ButtonSelectSFX.play()
+
+
+func _on_exit_button_mouse_entered() -> void:
+	exit_button.grab_focus()
+
+
+func _on_exit_button_focus_entered() -> void:
+	ButtonSelectSFX.play()
