f048065a2c546b390cd9e87dd6bf9266f506df64
Cameron <cben560@aucklanduni.ac.nz>
2025-03-15 17:27:44 +1300

    speedometer
    added speedometer

---
+++ speedometer.gd
@@ -0,0 +1,7 @@
+extends Label
+@onready var car: RigidBody3D = $"../Car"
+@onready var speedometer: Label = $"."
+
+
+func _process(delta):
+	speedometer.text = "Speed: %skm/h" % int(round((car.linear_velocity.length()) * 2))
