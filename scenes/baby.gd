fdc59fcf94c5dc1d49bc7235f354c21c464a8454
Geoff <geoffduk@gmail.com>
2025-03-15 20:16:44 +1300

    wow

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -1,5 +1,11 @@
 extends Area3D
 
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+func _physics_process(delta: float) -> void:
+	if (position.distance_to(player.position) > 100):
+		queue_free()
+
 func _on_body_entered(body: Node3D) -> void:
 	Global.score += (Global.speed_score_multiplier * 100)
 	queue_free()
--- scenes/cone.gd
+++ scenes/cone.gd
@@ -1,13 +1,7 @@
-extends Area3D
+extends RigidBody3D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
-
-
-func _on_body_entered(body: Node3D) -> void:
-	Global.speed_score_multiplier += 0.05
-	queue_free()
 
 func _physics_process(delta: float) -> void:
 	if (position.distance_to(player.position) > 100):
 		queue_free()
-	rotate_y(delta*1)
