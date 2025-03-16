903c1626329b15138ebe34dd3ebe33d527751b29
tabyrocket <abhijotmalhi@gmail.com>
2025-03-16 15:50:13 +1300

    Fixed main menu bug, also added universal reset

--- scenes/ui_controller.gd
+++ scenes/ui_controller.gd
@@ -2,14 +2,17 @@ extends Control
 @onready var game_over: Control = $game_over
 @onready var in_game_ui: Control = $in_game_UI
 @onready var score: Label = $game_over/VBoxContainer/score
+@onready var notifications: Control = $notifications
 
 func _process(delta: float) -> void:
 	if Global.is_dead:
 		score.text = "FINAL SCORE: %d" % Global.score
 		game_over.visible = true
+		notifications.visible = false
 		in_game_ui.visible = false
 	else:
 		game_over.visible = false
+		notifications.visible = true
 		in_game_ui.visible = true
 
 
