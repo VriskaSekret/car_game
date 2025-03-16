6c89b13d0a7816525b2497f5a27ac1f3099834b8
tabyrocket <abhijotmalhi@gmail.com>
2025-03-16 16:10:58 +1300

    Fixed annoying main menu button

--- scenes/ui_controller.gd
+++ scenes/ui_controller.gd
@@ -8,11 +8,11 @@ func _process(delta: float) -> void:
 	if Global.is_dead:
 		score.text = "FINAL SCORE: %d" % Global.score
 		game_over.visible = true
-		notifications.visible = false
+		#notifications.visible = false
 		in_game_ui.visible = false
 	else:
 		game_over.visible = false
-		notifications.visible = true
+		#notifications.visible = true
 		in_game_ui.visible = true
 
 
