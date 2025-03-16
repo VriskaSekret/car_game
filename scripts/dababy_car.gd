9d0d21c7302cfad418680b7334701586e1e1322d
Cameron <cben560@aucklanduni.ac.nz>
2025-03-16 16:11:20 +1300

    particles done

--- scripts/dababy_car.gd
+++ scripts/dababy_car.gd
@@ -15,6 +15,8 @@ var turn_input = 0
 @onready var body_mesh = $CarMesh/dababycar
 @onready var ground_ray = $CarMesh/RayCast3D
 
+@onready var engine_player: AudioStreamPlayer = $Engine
+@onready var drift_player: AudioStreamPlayer = $Drift
 @onready var cpu_particles_3d: CPUParticles3D = $CarMesh/CPUParticles3D
 
 
@@ -37,6 +39,7 @@ func _process(delta):
 	acceleration = 25.0 * (Global.speed_score_multiplier)
 	if not ground_ray.is_colliding() or Global.is_dead:
 		return
+	engine_player.pitch_scale = ((abs(Input.get_axis("brake", "accelerate"))/2) + 1)
 	if Input.is_key_pressed(KEY_SPACE):
 		turn_speed = 6.0
 		drift_direction_multiplier = -1
@@ -44,12 +47,19 @@ func _process(delta):
 		turn_speed = 3.0
 		drift_direction_multiplier = 1
 	if drift_direction_multiplier == -1 and Input.get_axis("steer_right", "steer_left") != 0:
-			acceleration += acceleration * 0.2
+		drift_player.play()
+		cpu_particles_3d.emitting = true
+		acceleration += acceleration * 0.2
+	else:
+		drift_player.stop()
+		cpu_particles_3d.emitting = false
 	speed_input = Input.get_axis("brake", "accelerate") * acceleration
 	if speed_input > 0:
 		turn_input = Input.get_axis("steer_right", "steer_left") * deg_to_rad(steering)
+		engine_player.volume_db = 5 * ((abs(Input.get_axis("brake", "accelerate"))/2) + 1)
 	elif speed_input < 0:
 		turn_input = Input.get_axis("steer_right", "steer_left") * -deg_to_rad(steering)
+		engine_player.volume_db = 5 * ((abs(Input.get_axis("brake", "accelerate"))/2) + 1)
 
 	
 	if linear_velocity.length() > turn_stop_limit:
