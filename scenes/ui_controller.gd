2c0824453b55da7eceae3136695e6a95d5464149
Geoff <geoffduk@gmail.com>
2025-03-16 13:51:13 +1300

    more ui changes

--- scenes/ui_controller.gd
+++ scenes/ui_controller.gd
@@ -1,1 +1,11 @@
 extends Control
+@onready var game_over: Control = $game_over
+@onready var in_game_ui: Control = $in_game_UI
+
+func _process(delta: float) -> void:
+	if Global.is_dead:
+		game_over.visible = true
+		in_game_ui.visible = false
+	else:
+		game_over.visible = false
+		in_game_ui.visible = true
